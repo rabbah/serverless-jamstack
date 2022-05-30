fcfbe7b2929128b610ec41e7618f9223d54216b1
Rodric Rabbah <rodric@gmail.com>
2022-05-30 10:48:04 -0400

    Remove CA cert env var. Rename package to match tutorial.

--- packages/cloud/getCoffee/index.js
+++ packages/cloud/getCoffee/index.js
@@ -1,24 +1,12 @@
 // The function's dependencies.
 const MongoClient = require('mongodb').MongoClient;
-const fs = require('fs');
-const path = require('path');
 
 // Function starts here.
 async function main() {
 
-// Locates the database cluster's CA cert environment variable, writes it to a .pem file and provides the file's location to the MongoDB client.
-
-    const DB_CERT = process.env['CA_CERT'].replace(/\\n/g, '\n');
-    const DATABASE_URL = process.env['DATABASE_URL'];
-    const uri = DATABASE_URL
-    const tlsCAFilePath = path.resolve(__dirname, 'certificate.pem');
-    fs.writeFileSync(tlsCAFilePath, DB_CERT, { encoding: 'utf8' });
-
 // MongoDB client configuration.
-    let client = new MongoClient(uri, {
-        tls: true,
-        tlsCAFile: tlsCAFilePath
-    });
+    const DATABASE_URL = process.env['DATABASE_URL'];
+    let client = new MongoClient(DATABASE_URL);
 
 // Instantiates a connection to the database and retrieves data from the `available-coffee` collection
     try {
--- packages/cloud/postEmail/index.js
+++ packages/cloud/postEmail/index.js
@@ -1,19 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
-const fs = require('fs');
-const path = require('path');
 
 async function main(args) {
-    const DB_CERT = process.env['CA_CERT'].replace(/\\n/g, '\n');
     const DATABASE_URL = process.env['DATABASE_URL'];
-    const uri = DATABASE_URL
-
-    const tlsCAFilePath = path.resolve(__dirname, 'certificate.pem');
-    fs.writeFileSync(tlsCAFilePath, DB_CERT, { encoding: 'utf8' });
-
-    let client = new MongoClient(uri, {
-        tls: true,
-        tlsCAFile: tlsCAFilePath
-    });
+    let client = new MongoClient(DATABASE_URL);
 
     let newEmail = args.email;
     try {
